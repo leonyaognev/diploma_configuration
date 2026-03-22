25462695996e45caaa3416d36a896a8bc27977c9
Leonid Ognev <penguin.ognev@gmail.com>
2026-03-23 01:41:13 +0700

    feat: add gitlab regestry settings

--- nix/modules/nginx.nix
+++ nix/modules/nginx.nix
@@ -18,5 +18,20 @@
         '';
       };
     };
+
+    virtualHosts."registry.git.osfb.dev" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:5005";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_ssl_server_name on;
+          proxy_pass_header Authorization;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+      };
+    };
   };
 }

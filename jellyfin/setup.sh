d75e29a0113b9ab71d6bb49cdec46a68afde4f17
Leonid Ognev <penguin.ognev@gmail.com>
2026-03-29 12:55:11 +0700

    feat: added jellyfin

---
+++ jellyfin/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p /tb_storage/jellyfin/config
+mkdir -p /tb_storage/jellyfin/cache
+mkdir -p /tb_storage/jellyfin/media
--- nix/modules/nginx.nix
+++ nix/modules/nginx.nix
@@ -4,6 +4,35 @@
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
+
+    # ===[ Git Lab ]===
+
+    virtualHosts."chlenify.ru" = {
+      forceSSL = true;
+      enableACME = true;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        extraConfig = ''
+          proxy_ssl_server_name on;
+          proxy_pass_header Authorization;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+      };
+
+      locations."/socket" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_ssl_server_name on;
+          proxy_pass_header Authorization;
+          proxy_set_header X-Forwarded-Proto https;
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+      };
+    };
+
     # ===[ Git Lab ]===
 
     virtualHosts."git.osfb.dev" = {
